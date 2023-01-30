6bea3bdfebfdc65b9d1dc4aab8fb49d952af8147
Anjani Kumar <99578509+ANJani9web@users.noreply.github.com>
2023-01-30 10:47:01 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+// Question 01
+let a = "Anjani"
+let b = 1
+console.log(a + b)
+
+// Question 02
+console.log(typeof (a + b))
+
+// Question 03
+const c = {
+  name1: "Harry",
+  section: 1,
+  isPrincipal: false
+}
+
+console.log(c)
+
+// Question 04
+c['friend'] = "Anjani"
+console.log(c)
+c['name1'] = "kumar"
+console.log(c)
+
+// Question 05
+const dictionary = {
+  appreciate : "recognize the full worth of",
+  ataraxia : "a state of freedom from emotional disturbance and anxiety",
+  yakka : "work, especially hard work"
+}
+console.log(dictionary)
+console.log(dictionary.yakka)
